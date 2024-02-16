1c54b995e55a3ffbcdc919003407e9cda2c17d0b
Gustavo Emmanuel Gonzalez Flores <gustavo.flores@vasscompany.com>
2024-02-16 01:33:50 -0600

    configuracion app.properties para mostrar una lista

--- src/main/java/com/vasscompany/webapp/springweb/controllers/PathVariableController.java
+++ src/main/java/com/vasscompany/webapp/springweb/controllers/PathVariableController.java
@@ -1,5 +1,9 @@
 package com.vasscompany.webapp.springweb.controllers;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,20 @@ import com.vasscompany.webapp.springweb.models.dto.ParamDto;
 @RestController
 @RequestMapping("api/var")
 public class PathVariableController {
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfFruits}")
+    private String[] fruits;
 
     @RequestMapping("/baz/{message}")
-    public ParamDto baz(@PathVariable(name = "message") String msj){
+    public ParamDto baz(@PathVariable(name = "message") String msj) {
         ParamDto param = new ParamDto();
         param.setMessage(msj);
 
@@ -22,11 +37,22 @@ public class PathVariableController {
     }
 
     @PostMapping("/create")
-    public User user(@RequestBody User user){
+    public User user(@RequestBody User user) {
         User usr = new User();
         usr.setName(user.getName());
         usr.setLastname(user.getLastname());
         usr.setEmail(user.getEmail());
         return usr;
+    }
+
+    @GetMapping("/values")
+    public HashMap<String,Object> mix(){
+        HashMap<String,Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("fruits", fruits);
+
+        return json;
     }
 }
