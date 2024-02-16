28664933a4804f4d3e601d0fb1e6bc7cf3ad3183
Gustavo Emmanuel Gonzalez Flores <gustavo.flores@vasscompany.com>
2024-02-16 00:11:41 -0600

    metodo post

--- src/main/java/com/vasscompany/webapp/springweb/controllers/PathVariableController.java
+++ src/main/java/com/vasscompany/webapp/springweb/controllers/PathVariableController.java
@@ -1,9 +1,12 @@
 package com.vasscompany.webapp.springweb.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vasscompany.webapp.springweb.models.User;
 import com.vasscompany.webapp.springweb.models.dto.ParamDto;
 
 @RestController
@@ -16,5 +19,14 @@ public class PathVariableController {
         param.setMessage(msj);
 
         return param;
+    }
+
+    @PostMapping("/create")
+    public User user(@RequestBody User user){
+        User usr = new User();
+        usr.setName(user.getName());
+        usr.setLastname(user.getLastname());
+        usr.setEmail(user.getEmail());
+        return usr;
     }
 }
