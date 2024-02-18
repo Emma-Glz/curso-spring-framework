973351abadd8cad24242fb0c4b5489a0654a8533
Gustavo Emmanuel Gonzalez Flores <gustavo.flores@vasscompany.com>
2024-02-18 08:36:30 -0600

    configurar otro properties

--- src/main/java/com/vasscompany/webapp/springweb/SpringWebApplication.java
+++ src/main/java/com/vasscompany/webapp/springweb/SpringWebApplication.java
@@ -2,8 +2,10 @@ package com.vasscompany.webapp.springweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:values.properties")
 public class SpringWebApplication {
 
 	public static void main(String[] args) {
